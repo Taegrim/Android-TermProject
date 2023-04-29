74019b9241865ecb31cd90abf5d7eb9ea8a457e8
Lee Dong Hyun <aopo3948@gmail.com>
2023-04-29 17:42:02 +0900

    타겟 업데이트를 일정 시간마다 하도록 변경
    적이 플레이어를 추격할 때는 항상 방향을 구할 필요는 없을 것
    얼추 따라가기만 해도 된다면 매번 방향을 구하지 않고 일정 시간마다 구하면 됨
    
    Enemy 는 frames 라는 변수를 가지고, UPDATE_FRAME 이라는 상수를 가짐
    update 가 30번 호출될때마다 플레이어를 기준으로 방향을 갱신해서 추격하도록 함
    현재 1초에 60프레임을 기준으로 했을 때 0.5초마다 방향을 갱신

--- Last Survivor/app/src/main/java/kr/ac/tukorea/sgp/s2018182024/lastsurvivor/game/Enemy/Enemy.java
+++ Last Survivor/app/src/main/java/kr/ac/tukorea/sgp/s2018182024/lastsurvivor/game/Enemy/Enemy.java
@@ -11,16 +11,19 @@ public class Enemy extends AnimationSprite {
     private float dx, dy;
     private float tx, ty;
     protected float speed;
+    private int frames;
+    private static final int UPDATE_FRAME = 30;
     private Player player;
+
     public Enemy(int resId, float x, float y, float width, float height, float fps, int frameCount) {
         super(resId, x, y, width, height, fps, frameCount);
 
         MainScene scene = (MainScene) BaseScene.getTopScene();
         player = scene.player;
+        targetUpdate();
     }
 
-    @Override
-    public void update() {
+    private void targetUpdate() {
         tx = player.getX();
         ty = player.getY();
         float directionX = tx - this.x;
@@ -28,6 +31,15 @@ public class Enemy extends AnimationSprite {
         double radian = Math.atan2(directionY, directionX);
         dx = (float) (speed * Math.cos(radian));
         dy = (float) (speed * Math.sin(radian));
+    }
+
+    @Override
+    public void update() {
+        frames +=1;
+        if(frames == UPDATE_FRAME){
+            targetUpdate();
+            frames = 0;
+        }
 
         float time = BaseScene.frameTime;
         x += dx * time;
