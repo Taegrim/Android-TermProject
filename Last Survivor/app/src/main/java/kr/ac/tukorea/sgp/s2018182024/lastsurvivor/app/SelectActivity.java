d982f9b9f90f826079d1f909650d11b238782a2d
Lee Dong Hyun <aopo3948@gmail.com>
2023-05-15 21:45:40 +0900

    프레임 레이아웃으로 이미지 버튼 아래에 이미지, 텍스트 배치
    FrameLayout, LinearLayout 을 조합하여
    이미지버튼, 이미지, 텍스트를 중첩하여 배치
    이미지 버튼 클릭시 Log 메세지 출력

--- Last Survivor/app/src/main/java/kr/ac/tukorea/sgp/s2018182024/lastsurvivor/app/SelectActivity.java
+++ Last Survivor/app/src/main/java/kr/ac/tukorea/sgp/s2018182024/lastsurvivor/app/SelectActivity.java
@@ -3,11 +3,22 @@ package kr.ac.tukorea.sgp.s2018182024.lastsurvivor.app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import kr.ac.tukorea.sgp.s2018182024.lastsurvivor.R;
 
 public class SelectActivity extends AppCompatActivity {
+
+    private static final String TAG = SelectActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select);
+    }
+
+    public void onBtnSelectAbility(View view) {
+        Log.d(TAG, "Select Ability!");
     }
 }
