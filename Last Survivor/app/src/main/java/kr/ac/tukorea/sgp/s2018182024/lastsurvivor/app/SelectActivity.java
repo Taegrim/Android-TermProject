5d783ff6308fef04e322ef03a7d466915e76a37c
Lee Dong Hyun <aopo3948@gmail.com>
2023-05-15 21:11:49 +0900

    선택지 액티비티 추가
    선택지 액티비티 추가 및 버튼, 이미지 배치

---
+++ Last Survivor/app/src/main/java/kr/ac/tukorea/sgp/s2018182024/lastsurvivor/app/SelectActivity.java
@@ -0,0 +1,13 @@
+package kr.ac.tukorea.sgp.s2018182024.lastsurvivor.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
